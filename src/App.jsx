e3f5f62be81253f07a576efca349dcf92d270611
PierreSarciat <phsarciat@mailo.com>
2025-09-29 20:58:41 +0200

    créations pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import { Outlet } from "react-router-dom";
-//import Header from './components/header.jsx';
+import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 
 function App() {
   return (
     <div className='App'>
-      {/*{<Header />*/}         
+      <Header />       
       <main>
         <Outlet />       
       </main>
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import Home from './pages/home.jsx';
+import FicheLogement from './pages/fiche-logement.jsx';
+import Erreur from './pages/page-erreur.jsx';
+import Apropos from './pages/Apropos.jsx';
+
 
 
 const router = createBrowserRouter([
@@ -9,7 +13,23 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
-      { path: '/', element: <Home /> },
+      { path: '/', 
+        element: <Home/>
+
+       },
+       {
+        path:'/fiche-logement',
+        element:<FicheLogement/>
+       },
+       {
+        path:'/page-erreur',
+        element:<Erreur/>
+       },
+       {
+        path:'/Apropos',
+        element:<Apropos/>
+       },
+      
      
     ],
   },
