593aac01fe28c2331236a13284da71e6e92fe0c1
PierreSarciat <phsarciat@mailo.com>
2025-10-01 22:00:56 +0200

    style scss

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,84 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      Home works
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
+
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
+
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
+
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
+
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
+
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
+
+      <a href="#">
+        <article className="card">
+          <img src="" alt="" />
+          <div className="card-content">
+            <div className="card-txt">
+              <h3 className="card-title">Card</h3>
+            </div>
+          </div>
+        </article>
+      </a>
     </div>
   );
 };
 
-export default Home
+export default Home;
