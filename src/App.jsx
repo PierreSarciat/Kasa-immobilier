ade272b9922c66c7f0498171e46a8a51028c08bd
PierreSarciat <phsarciat@mailo.com>
2025-10-01 22:03:24 +0200

    style scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Outlet } from "react-router-dom";
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.scss'
 import Router from './router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
