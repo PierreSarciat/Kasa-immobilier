709a554ecca5991d88bb6565c753ff747f97ee49
PierreSarciat <phsarciat@mailo.com>
2025-10-05 11:37:17 +0200

    mise en place du carrousel

--- src/pages/fiche-logements.jsx
+++ src/pages/fiche-logements.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import "./fiche-logements.jsx"
+import { useParams } from "react-router-dom";
+import "./fiche-logements.scss"
+import data from "../../public/logements.json";
+import Carrousel from "../components/carrousel.jsx";
 
 const FicheLogement = () => {
-    return (
-        <div>
-          Fiche Logement  
-        </div>
-    );
+  const { id } = useParams();
+  const logement = data.find(item => item.id === id);
+
+  if (!logement) {
+    return <div>Logement introuvable</div>;
+  }
+
+  return (
+    <div className="logement-page">
+      <Carrousel pictures={logement.pictures} />
+
+      <h1>{logement.title}</h1>
+      <p>{logement.location}</p>
+    </div>
+  );
 };
+
 
 export default FicheLogement
