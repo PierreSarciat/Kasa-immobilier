a75c5674aa7521bd9f8f695653a25b2240eda880
PierreSarciat <phsarciat@mailo.com>
2025-10-11 15:31:02 +0200

    page erreur mobile

--- src/pages/page-erreur.jsx
+++ src/pages/page-erreur.jsx
@@ -7,7 +7,7 @@ const Erreur=() => {
     return (
         <div className='erreur-page'>
            <p>404</p>
-           <p>Oups! La page que vous demandez n' existe pas</p>
+           <p><span>Oups! La page que</span><span>vous demandez n' existe pas</span> </p>
             <Link to="/">Retour sur la page d'accueil</Link>
         </div>
     );
