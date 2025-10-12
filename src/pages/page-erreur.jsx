427433572482ab8dd3cafe81024d357145d81f7a
PierreSarciat <phsarciat@mailo.com>
2025-10-12 22:12:50 +0200

    modif p page erreur

--- src/pages/page-erreur.jsx
+++ src/pages/page-erreur.jsx
@@ -7,7 +7,7 @@ const Erreur=() => {
     return (
         <div className='erreur-page'>
            <p>404</p>
-           <p><span>Oups! La page que</span><span>vous demandez n' existe pas</span> </p>
+           <p><span>Oups! La page que</span><span> vous demandez n' existe pas</span> </p>
             <Link to="/">Retour sur la page d'accueil</Link>
         </div>
     );
